0f5f926cb38fe7afb6747cbfbb5b81e556cb1e36
Claire135 <186298887+Claire135@users.noreply.github.com>
2025-02-19 22:48:00 +0100

    added additional features such as score keeping and game over with a score of 3. Added create_player, which had previously been in player class

--- lib/game.rb
+++ lib/game.rb
@@ -1,41 +1,73 @@
 class Game
     include GameLogic
   
-    def initialize(player1, player2, board)
-      @player1 = player1
-      @player2 = player2
-      @board = board
+    def initialize
+      @board = Board.new
+
+      loop do
+        @player1 = create_player("X")
+        @player2 = create_player("O")
+    
+        if @player1.name == @player2.name
+          puts "Player names must be different. Please enter again."
+        else
+          break
+        end
+      end
+    end
+
+    def create_player(piece_kind)
+      loop do
+        puts "Enter name for player 1 (#{piece_kind}'s): "
+        name = gets.chomp.strip
+  
+        return Player.create(name, piece_kind)
+      end
     end
   
     def play
       loop do
-        puts "#{@player1.name}'s turn!"
-        place_piece(@player1, @board)
-        @board.display_board
-        if check_win(@player1)
-          puts "#{@player1.name} wins!"
-          @player1.score += 1
-          break
-        end
-        puts "#{@player2.name}'s turn! "
-        place_piece(@player2, @board)
-        @board.display_board
-        if check_win(@player2)
-          puts "#{@player2.name} wins!"
-          @player2.score += 1
-          break
-        end
-  
-        if @player1.score == 3 || @player2.score == 3
-          puts "Game over!"
-           if @player1.score > @player2.score
-             puts "#{@player1.name} wins!"
-           else puts "#{@player2.name} wins!"
-           end
-          break
-        
+        [@player1, @player2].each do |player|
+          puts "#{player.name}'s turn!"
+          place_piece(player, @board)
+          @board.display_board
+    
+          if check_win(player)
+            puts "#{player.name} wins!"
+            player.score += 1
+            puts "#{@player1.name}'s score: #{@player1.score}, #{@player2.name}'s score: #{@player2.score}"
+    
+            if end_game
+              return  # Stop game loop
+            else
+              reset_board
+              return
+            end
+          elsif board_full?
+            puts "It's a draw! You both played a terrible game."
+            reset_board
+            return
+          end
         end
       end
+    end
+
+    def reset_board
+      puts "Winner is first to reach 3!"
+      @board = Board.new
+      play
+    end
+
+    def end_game
+        if @player1.score == 3 || @player2.score == 3
+          puts "Game over!"
+          if @player1.score > @player2.score
+            puts "#{@player1.name} is the ultimate winner!"
+          else puts "#{@player2.name} is the ultimate winner!"   
+          end  
+          return true  # This ensures the loop stops
+        end
+        false  # The game continues
     end
   
     def check_win(player)
@@ -51,4 +83,4 @@ class Game
         combination.all? { |index| flat_board[index] == player.piece_kind }
       end
     end
-  end
+end
